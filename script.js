ac228e6c3e0897e8da5c2ff054512186a8dafae1
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-10-04 08:23:43 +0100

    commit message

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+let num1 = 0; // when the user inputs the first number
+let num2 = 0; // when the user inputs the second number
+let para = "";
+let ans =0;
+let opera = "";
+let check = 0 ; //checks if any operator has been clicked
 const answer = document.getElementById("answer-space")
 
 
 function reset(){
-    answer.textContent =0
+    location.reload()
 }
