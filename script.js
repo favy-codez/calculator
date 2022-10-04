909b732d898384b0005c48e73a460d8415580908
favy-codez <107569845+favy-codez@users.noreply.github.com>
2022-10-04 09:25:55 +0100

    commit message

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ let opera = "";
 let check = 0 ; //checks if any operator has been clicked
 const answer = document.getElementById("answer-space")
 
-
+// C button
 function reset(){
     location.reload()
+}
+
+//
+for(let i=0; i<10; i++){
+    const buttonId = document.getElementById("btn-" + i)
+    buttonId.addEventListener ("click", function(){
+        para= para + i;
+        answer.innerHTML = para;
+    })
 }
